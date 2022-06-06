ac08c9828bde8f21e8d488703589a3ef5a9b6dfe
Caroline Lins <carolineglins@gmail.com>
2022-06-06 10:14:58 -0300

    Configurações no banco de dados

--- src/infrastructure/internal/database/postgresql/config/config.ts
+++ src/infrastructure/internal/database/postgresql/config/config.ts
@@ -1,5 +1,6 @@
 import { Options } from "sequelize"
 
+const options: Options = { dialect: 'postgres', logging: false, host: 'ec2-54-211-255-161.compute-1.amazonaws.com', port: 5432, timezone: '-03:00', define: { timestamps: true }, dialectOptions: { ssl: { rejectUnauthorized: false } } }
 const optionsDev: Options = { dialect: 'postgres', logging: false, host: '127.0.0.1', port: 5433, timezone: '-03:00', define: { timestamps: true } }
 
 const development = {
@@ -9,4 +10,11 @@ const development = {
   options: optionsDev
 }
 
-export default development
+const production = {
+  database: 'd8q2m40ggj9dur',
+  username: 'qzxrnpltggymhn',
+  password: '5985c08b495eb56b20c13ab9ce12ea2bcfcbb70cbcd84eb47621760481b20f13',
+  options: options
+}
+
+export default production
